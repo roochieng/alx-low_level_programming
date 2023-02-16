b94438f25f6b6cc03c9ba7e693805e59d670264f
Robley <49204011+roochieng@users.noreply.github.com>
2023-02-16 10:01:38 -0800

    Write a C program that prints the size of various types on the computer it is compiled and run on.

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	printf("Size of a char: %d byte(s)\n");
+	printf("Size of an int: %d byte(s)\n");
+	printf("Size of a long int: %d  byte(s)\n");
+	printf("Size of a long long int: %d byte(s)\n");
+	printf("Size of a float: %d byte(s)\n");
+	return (0);
+}
