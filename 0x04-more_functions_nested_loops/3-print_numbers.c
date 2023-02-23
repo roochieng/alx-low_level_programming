17be7adfbc91ea954227412136079af3f95c68b4
roochieng <49204011+roochieng@users.noreply.github.com>
2023-02-23 15:31:27 +0000

    Print 0-9 without printf

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_numbers - print numbers from 0-9
+ *
+ * Return: 0
+ */
+void print_numbers(void)
+{
+	for (i = 0; i <= 9)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+	return (0);
+}
