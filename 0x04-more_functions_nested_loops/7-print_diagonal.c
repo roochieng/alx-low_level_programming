6bb94417fc8beded38fe522c9e3c9aa7c03670ab
roochieng <49204011+roochieng@users.noreply.github.com>
2023-02-23 17:40:46 +0000

    print diagonal line

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,14 +9,20 @@ void print_diagonal(int n)
 {
 	int i, spc;
 
-	for (i = 0; i <= n; i++)
+	if (i <= 0)
 	{
-		_putchar('\\');
-
-		for (spc = 0; spc < i; spc++)
-		{
-			_putchar(' ');
-		}
 		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i <= n; i++)
+		{
+			for (spc = 0; spc < i; spc++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
 	}
 }
