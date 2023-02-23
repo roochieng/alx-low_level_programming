ffe10c8ccf23dffbd55b52a5bff7d885d9a6c7b8
roochieng <49204011+roochieng@users.noreply.github.com>
2023-02-23 15:56:37 +0000

    Print numbers apart from 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_most_numbers - numbers from 0-9 except 2 and 4
+ *
+ * Return: 0
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9' i++)
+	{
+		if (i == '2' || i == '4')
+		{
+			break;
+		}
+		_putchar(i);
+	}
+	return (0);
+}
