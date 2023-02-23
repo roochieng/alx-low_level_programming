87b264d72cfea8fc11f35c699de683d999f1d2bc
roochieng <49204011+roochieng@users.noreply.github.com>
2023-02-23 17:14:13 +0000

    print line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -10,16 +10,9 @@ void print_line(int n)
 {
 	int i;
 
-	if (n <= 0)
+	for (i = 0; i < n; i++)
 	{
-		_putchar('\n');
-	}
-	else
-	{
-		for (i = 0; i < n; i++)
-		{
-			_putchar('_');
-		}
+		_putchar('_');
 	}
 	_putchar('\n');
 }
