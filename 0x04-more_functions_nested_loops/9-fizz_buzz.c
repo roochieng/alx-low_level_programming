5330ac9c1fdda7ebb747566cb88c408af033ef55
roochieng <49204011+roochieng@users.noreply.github.com>
2023-02-23 19:26:09 +0000

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,30 +7,27 @@
 int fizzbuzz(void)
 {
 	int i;
-
+	
 	for (i = 1; i <= 100; i++)
 	{
-		if (i == 100)
+		if (i % 3 == 0 || i % 5 == 0)
 		{
-			printf("Buzz");
-		}
-		else if ((i % 3 == 0) && (i % 5 == 0))
+			if (i % 3 == 0)
+			{
+				printf("Fizz");
+			}
+			if (i % 5 == 0)
+			{
+				printf("Buzz");
+			}
+		} else
 		{
-			printf("FizzBuzz ");
+			printf("%d", i);
 		}
-		else if (i % 3 == 0)
-		{
-			printf("Fizz ");
-		}
-		else if (i % 5 == 0)
-		{
-			printf("Buzz ");
-		}
-		else
-		{
-			printf("%d ", i);
-		}
+		if (i != 100)
+			printf(" ");
 	}
 	printf("\n");
 	return (0);
 }
+
