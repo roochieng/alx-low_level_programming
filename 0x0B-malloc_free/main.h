71ed5bbc4c5bbf04e4b583dbba3ae1297f557cc6
roochieng <49204011+roochieng@users.noreply.github.com>
2023-03-13 16:43:55 +0000

    Write a function that concatenates all the arguments of your program.

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -57,5 +57,7 @@ char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
 char *str_concat(char *s1, char *s2);
 void free_grid(int **grid, int height);
+char *argstostr(int ac, char **av);
+char **strtow(char *str);
 
 #endif
