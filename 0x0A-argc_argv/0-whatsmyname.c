49676f3a2000aca699f2260d18f45b0c796cecc9
roochieng <49204011+roochieng@users.noreply.github.com>
2023-03-14 17:32:29 +0000

    Write a program that prints its name, followed by a new line.

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * main - main funtion that prints itself
+ * @argv: arguments
+ * @argc: argument idexes
+ * Return: 0
+ */
+void main(int argc, char *argv[])
+{
+	if (argc > 0)
+	{
+		printf("%s\n", argv[0]);
+	}
+	return (0)
+}
