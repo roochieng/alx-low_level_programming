06f1b597529703e1e18c8f9a3e5677337b884aa8
roochieng <49204011+roochieng@users.noreply.github.com>
2023-05-22 16:55:06 +0000

    Create a script that creates a dynamic library called liball.so from all the .c files that are in the current directory.

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 gcc -Wall -pedantic -Werror -Wextra -c *.c
-gcc *.o -shared -o libdynamic.so
+gcc *.o -shared -o liball.so
 gcc -Wall -pedantic -Werror -Wextra -L. 0-main.c -ldynamic -o len
 
 sudo cp libdynamic.so /usr/lib
